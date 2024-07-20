18dac9d826ace588f8a28f3dcd0ae8e230985bb6
Thea <thea.joy@outlook.com>
2024-07-20 11:22:16 +0200

    build: add tailwind
    Signed-off-by: Thea <thea.joy@outlook.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,14 @@
-<x-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-</x-layout>
+    <title>{{ config('app.name') }}</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body class="">
+</body>
+</html>
+<?php
