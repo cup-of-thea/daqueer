a1c8f72ebf01960c5bce69911faddc146121bb8f
Thea <115863819+cup-of-thea@users.noreply.github.com>
2024-07-28 10:53:19 +0200

    feat(front): make responsive menu dynamic (#9)
    * feat(front): make responsive menu dynamic
    
    allows users to show-hide menu, and still being able to see the rest of the contents
    
    * feat(navigation): change responsive navigation for real pages
    
    allows to actually use the navigation on responsive devices

--- app/Livewire/Navigation.php
+++ app/Livewire/Navigation.php
@@ -6,4 +6,15 @@ use Livewire\Component;
 
 class Navigation extends Component
 {
+    public bool $showMenu = false;
+
+    public function openMenu(): void
+    {
+        $this->showMenu = true;
+    }
+
+    public function closeMenu(): void
+    {
+        $this->showMenu = false;
+    }
 }
