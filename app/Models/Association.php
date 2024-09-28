a9e388d932e9bcc6aa7c223af951d01eb1194418
Thea <thea.joy@outlook.com>
2024-09-28 16:18:28 +0200

    feat(editions): add create and list editions

--- app/Models/Association.php
+++ app/Models/Association.php
@@ -5,13 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Association extends Model
 {
-    use HasFactory, HasLinks;
+    use HasFactory, HasLinks, HasSlug;
 
     public function editions(): BelongsToMany
     {
         return $this->belongsToMany(Edition::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
     }
 }
--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -5,13 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Guest extends Model
 {
-    use HasFactory, HasLinks;
+    use HasFactory, HasLinks, HasSlug;
 
     public function editions(): BelongsToMany
     {
         return $this->belongsToMany(Edition::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('handle')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
     }
 }
